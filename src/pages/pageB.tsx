37f5908aa4876f42061b73c52171605e312c1cf9
top16Dev <topdev.1020@gmail.com>
2022-05-07 21:41:07 +0900

    Change B

--- src/pages/pageB.tsx
+++ src/pages/pageB.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const PageB = () => 
   <div>
-    <h2>Hello from page B</h2>
+    <h2>Hello from page B1</h2>
     <br />
     <Link to="/">Navigate to Page A</Link>
   </div>
